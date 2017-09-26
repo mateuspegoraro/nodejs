51cee31e366fe9232c8544cc8150307e71009146
Mateus <mateus.pegoraro@perceptvision.com.br>
2017-09-26 17:30:25 -0300

    iniciando com express

--- produtos.js
+++ produtos.js
@@ -4,10 +4,10 @@ var server = http.createServer(function(req,res){
     if(req.url =="/produtos"){
       res.end("<html><body>listando os produtos da loja</body>");
     } else {
-      res.end("<html><body>home da casa do codigo</body></html>");
+      res.end("<html><body>home do site</body></html>");
     }
 });
 
-server.listen(3000);
+server.listen(3000, "localhost");
 
 console.log("servidor ta rodando");
