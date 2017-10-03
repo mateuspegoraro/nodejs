505a16448e20dec75353be730138f10edd68ff8f
Mateus <mateus.pegoraro@perceptvision.com.br>
2017-10-03 11:45:07 -0300

    nodemon e ejs

--- produtos.js
+++ produtos.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req,res){
     if(req.url =="/produtos"){
-      res.end("<html><body>listando os produtos da loja</body>");
+      res.end("<html><body>listando os produtos da loja</body></html>");
     } else {
       res.end("<html><body>home do site</body></html>");
     }
